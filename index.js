6db3c9e2c8273a14a21f79cde87c8201268c4630
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-11-03 14:56:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,30 @@ app.get("/", (req, res) => {
   });
 });
 
+app.post("/", (req, res) => {
+  const body = req.body;
+  const x = parseInt(body.x ?? 0);
+  const y = parseInt(body.y ?? 0);
+  let result = 0;
+
+  switch (body.operation_type) {
+    case "addition":
+      result = x + y;
+      break;
+    case "subtraction":
+      result = x - y;
+      break;
+    case "multiplication":
+      result = x * y;
+      break;
+  }
+
+  return res.json({
+    slackUsername: "imranabdulmalik01",
+    operation_type: body.operation_type,
+    result: result,
+  });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
